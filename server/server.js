e39ffc5edea2c664b7612cbcdfc57a25b91b689a
Tyler Fabian <tylerf808@gmail.com>
2021-08-06 14:40:52 -0400

    Created server file and server side folders

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+});
